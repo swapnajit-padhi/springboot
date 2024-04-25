bcc009c8085a199924b5c77550c0d91a9a600243
swapnajit-padhi <158247685+swapnajit-padhi@users.noreply.github.com>
2024-04-25 17:18:48 +0530

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "This is a SpringBoot Static Web Application");
+        model.addAttribute("title", "This is a SpringBoot Static Web App");
         model.addAttribute("msg", "Application Is Deployed To Kuberneets");
         return "index";
     }
